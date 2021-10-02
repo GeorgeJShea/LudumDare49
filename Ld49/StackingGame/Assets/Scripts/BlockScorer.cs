9c0a80ececf96c1f6134bed1c24c2a9c1923b542
WaylundIsAwesome <91765045+WaylundIsAwesome@users.noreply.github.com>
2021-10-02 12:37:46 -0600

    fixed color lines, camera climbs

--- Ld49/StackingGame/Assets/Scripts/BlockScorer.cs
+++ Ld49/StackingGame/Assets/Scripts/BlockScorer.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class BlockScorer : MonoBehaviour
 {
     private GameManagement gameManagement;
-    private bool didSmoke = false;
     public GameObject smokePrefab;
 
     // Start is called before the first frame update
@@ -21,14 +20,20 @@ public class BlockScorer : MonoBehaviour
         
     }
 
+    private void checkCameraPosition(float blockPosition) {
+        GameObject camera = GameObject.FindWithTag("MainCamera");
+        Vector3 cameraPosition = camera.transform.position;
+        if (blockPosition > cameraPosition.y) {
+            camera.transform.position = new Vector3(cameraPosition.x, blockPosition, 0f);
+        }
+    }
+
     private void OnCollisionEnter2D(Collision2D other) {
         if (other.gameObject.CompareTag("Block")) {
             gameManagement.updateScore(10);
-            if (!didSmoke) {
-                Vector3 smokePosition = new Vector3(transform.position.x, transform.position.y-0.35f, 0f);
-                Instantiate(smokePrefab, smokePosition, Quaternion.identity);
-                didSmoke = true;
-            }
+            Vector3 smokePosition = new Vector3(transform.position.x, transform.position.y-0.35f, 0f);
+            Instantiate(smokePrefab, smokePosition, Quaternion.identity);
+            checkCameraPosition(other.transform.position.y);
         }
     }
 }
